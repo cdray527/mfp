b4fd8160c9bb83f9c7869a48498c7cdc8ad7b031
cdray527 <cdray085@hotmail.com>
2024-08-21 16:52:34 +0800

    Fix: add debug flag in container.yml & rerun pipeline

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingPage from './components/MarketingPage';
 export default () => {
     return (
         <div>
-            <h1>Test !</h1>
+            <h1>Test !!</h1>
             <MarketingPage />
         </div>
     );
