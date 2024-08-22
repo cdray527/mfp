96b9f97bd4483715d5a61825c3c363ad340e5790
cdray527 <cdray085@hotmail.com>
2024-08-22 17:20:23 +0800

    Update docker config & auth package

---
+++ packages/auth/config/webpack.common.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+           // loader is to tell Webpack process some different files as we start to import them into our project 
+           {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react',
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-runtime' 
+                        ],
+                    }
+                }
+           }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ]
+};
---
+++ packages/auth/config/webpack.dev.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        host: "0.0.0.0",
+        port: 8082,
+        historyApiFallback: true,
+        watchOptions: {
+            poll: 1000,
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'auth',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './AuthPage': './src/bootstrap'
+            },
+            shared: packageJson.dependencies,
+        }),
+    ]
+};
+
+module.exports = merge(commonConfig, devConfig);
